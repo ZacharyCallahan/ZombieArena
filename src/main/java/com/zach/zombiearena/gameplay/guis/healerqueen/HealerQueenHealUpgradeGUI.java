d644c623bc3ef3cde74f737cb9244f8ac8189530
FuturePinger <zachcallahan90@gmail.com>
2022-08-29 03:27:23 -0400

    Test

--- src/main/java/com/zach/zombiearena/gameplay/guis/healerqueen/HealerQueenHealUpgradeGUI.java
+++ src/main/java/com/zach/zombiearena/gameplay/guis/healerqueen/HealerQueenHealUpgradeGUI.java
@@ -61,8 +61,11 @@ public class HealerQueenHealUpgradeGUI {
         if (getPlayersHealerQueenHealUpgradeLevel(player) == 4) {
             return config.getHealerQueenHealUpgradeLevelFourHeal();
         }
+        player.hasPermission("zombiearena.healer-queen.heal-upgrade.1");
         return config.getHealerQueenHeal();
+
     }
+
 
     public void refreshMenu(Player player) {
         ZombieArena.getMenuHandler().closeMenu(player);
